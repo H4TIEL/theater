45d329a3ac35a7164d86a564926f42f5b0a349fd
h4tiel <danielh4t@pm.me>
2022-08-02 21:27:16 +0200

    update title

--- content-script.js
+++ content-script.js
@@ -1,7 +1,7 @@
-const theater = 'Theater mode (t)'
+const title = 'Default view (t)'
 const selector = document.querySelector('.ytp-size-button');
 // check if selector is not null and not in Theater mode
-if (selector && selector.title.includes('Theater'))
+if (selector && selector.title.includes(title))
 {
     selector.click();
     console.log('Theater mode enabled')
--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 let text = document.getElementById('theater');
 
 text.addEventListener('click', async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: console.log('popup'),
-    });
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: console.log('popup'),
   });
+});
